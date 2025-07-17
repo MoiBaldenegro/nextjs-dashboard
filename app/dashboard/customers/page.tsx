cf3e1c03073b7292b161d07d6eec65d27250d152
Your Name <you@example.com>
2025-07-16 23:05:38 -0600

    add async data fetching for customers and dashboard pages

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,9 @@
-export default function CustomerPage() {
+import { fetchCustomers } from "@/app/lib/data";
+
+export default async function  CustomerPage() {
+  const response = await fetchCustomers();
+  console.log(response);
+  
   return (
     <h1>CUSTOMER PAGE</h1>
   );
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,9 @@
-export default function Dasboard(){
+import { fetchRevenue } from "../lib/data"
+
+export default async function Dasboard(){
+
+    const response = await fetchRevenue();
+    console.log(response);
 return (
     <h1>DASHBOARD</h1>
 )
